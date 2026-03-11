06e80d701d7dbcc0b612294e187fe7ae1fae46de
IvanBanny <falco.zebrine@gmail.com>
2026-03-11 14:28:19 +0100

    feat(vscode): minor vscode config updates

--- .config/nvim/lua/config/vscode_keymaps.lua
+++ .config/nvim/lua/config/vscode_keymaps.lua
@@ -6,7 +6,11 @@ map("n", "<leader>w", function() vscode.action("workbench.action.files.save") en
 
 -- Navigation
 map("n", "<leader>,", function() vscode.action("workbench.action.showAllEditors") end)
-map("n", "<leader>e", function() vscode.action("workbench.action.toggleSidebarVisibility") end)
+map("n", "<leader>e", function()
+    vscode.action("workbench.action.toggleSidebarVisibility")
+    vscode.action("workbench.files.action.focusFilesExplorer")
+end)
+map("n", "<leader>t", function() vscode.action("workbench.action.terminal.toggleTerminal") end)
 map("n", "<leader>bd", function() vscode.action("workbench.action.closeActiveEditor") end)
 map("n", "<leader><leader>", function() vscode.action("workbench.action.quickOpen") end)
 map("n", "<leader>sg", function() vscode.action("workbench.action.findInFiles") end)
@@ -23,4 +27,5 @@ map("n", "<leader>cr", function() vscode.action("editor.action.rename") end)
 map("n", "<leader>cs", function() vscode.action("workbench.action.gotoSymbol") end)
 map("n", "<leader>gd", function() vscode.action("editor.action.revealDefinition") end)
 map("n", "<leader>gr", function() vscode.action("editor.action.goToReferences") end)
+map("n", "<leader>gi", function() vscode.action("editor.action.goToImplementation") end)
 map({"n", "v"}, "<C-n>", function() vscode.action("editor.action.addSelectionToNextFindMatch") end)
